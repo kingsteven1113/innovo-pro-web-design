f9e072bc08343f12c00defc334008b8ec4740881
Steven King <kingsteven1113@gmail.com>
2025-01-01 18:06:04 -0500

    Hero basically done and now working on the next few sections

--- innovo-pro-web-design/src/Components/LandingPage/LandingPage.jsx
+++ innovo-pro-web-design/src/Components/LandingPage/LandingPage.jsx
@@ -1,19 +1,92 @@
 import React from 'react';
-import './LandingPage.css'; 
+import './LandingPage.css';
+import BackgroundTube from '../../assets/BackgroundTubeImage.svg'
+import AdobeLogo  from '../../assets/Adobe-Logo.svg'
+import FigmaLogo  from '../../assets/Figma-Logo.svg' 
+import HTMLLogo  from '../../assets/HTML-Logo.svg' 
+import CSSLogo  from '../../assets/CSS-Logo.svg' 
+import JSLogo  from '../../assets/JS-Logo.svg' 
+import ReactLogo  from '../../assets/React-Green-Logo.svg'
+import Computer from '../../assets/Computer.svg'
+import HeaderBreadcrumb1 from '../../assets/HeaderBreadcrumb1.svg'
+import Discuss from '../../assets/Discuss.svg'
+import Diagnose from '../../assets/Diagnose.svg'
+import Deploy from '../../assets/Deploy.svg'
 
 const LandingPage = () => {
   return (
     <>
-    <div className='HeroSection'>
+    <section className='HeroSection'>
+      <div className='HeroBackgroundImage'>
+        <img src={BackgroundTube} alt="" />
+      </div>
+      <div className='ComputerDesign'>
+        <img src={Computer} alt="" />
+      </div>
+      <div className='HeroSectionGrid'>
+        <div className='HeroSectionBox LogoInBox'> <img className='BackgroundLogos' src={FigmaLogo} alt="" /></div>
+        <div className='HeroSectionBox'></div>
+        <div className='HeroSectionBox'></div>
+        <div className='HeroSectionBox'></div>
+        <div className='HeroSectionBox LogoInBox'><img className='BackgroundLogos' src={HTMLLogo} alt="" /></div>
+        <div className='HeroSectionBox LogoInBox'><img className='BackgroundLogos' src={AdobeLogo} alt="" /></div>
+        <div className='HeroSectionBox'></div>
+        <div className='HeroSectionBox'></div>
+        <div className='HeroSectionBox'></div>
+        <div className='HeroSectionBox LogoInBox'><img className='BackgroundLogos' src={JSLogo} alt="" /></div>
+        <div className='HeroSectionBox LogoInBox'><img className='BackgroundLogos' src={ReactLogo} alt="" /></div>
+        <div className='HeroSectionBox'></div>
+        <div className='HeroSectionBox'></div>
+        <div className='HeroSectionBox'></div>
+        <div className='HeroSectionBox LogoInBox'><img className='BackgroundLogos' src={CSSLogo} alt="" /></div>
+        <div className='HeroSectionBox'></div>
+        <div className='HeroSectionBox'></div>
+        <div className='HeroSectionBox'></div>
+        <div className='HeroSectionBox'></div>
+        <div className='HeroSectionBox'></div>
+      </div>
       <div className='HeroTopLayer'>
       <div className='HeroHeaders'>
         <h3 className='CompanyName'>Innovo Pro Web Design</h3>
         <h1 className='H1'>We Build Websites</h1>
       </div>
       <p className='HeroParagraph'>Helping the world discover you!</p>
-      <a className='CTA' href="/contact">Book a Call!</a>
+      <div className='HeroCTA'><a className='CTA' href="/contact">Book a Call!</a></div>
       </div>
-    </div>
+     
+    </section>
+    <section>
+      <div>
+        <div className='ProcessSectionHeader'>
+          <h2 className='ProcessHeader'>Our Process</h2>
+          <img src={HeaderBreadcrumb1} alt="" />
+        </div>
+        <div className='ProcessDiagram'>
+          <div className='Process-Item LogoInBox'>
+            <img src={Discuss} alt="" />
+            <p>Discuss</p>
+          </div>
+          <div className='Process-Item LogoInBox'>
+            <div className='MultiLogo'><img src="" alt="" /></div>
+            <p>Design</p>
+          </div>
+          <div className='Process-Item LogoInBox'>
+          <div className='MultiLogo'><img src="" alt="" /></div>
+            <p>Develop</p>
+          </div>
+          <div className='Process-Item LogoInBox'>
+            <img src={Diagnose} alt="" />
+            <p>Diagnose</p>
+          </div>
+          <div className='Process-Item LogoInBox'>
+            <img src={Deploy} alt="" />
+            <p>Deploy</p>
+          </div>
+        </div>
+        <img src="" alt="" />
+          <p>We have developed a smooth clean procces from an idea for a website to a fully built website that is ready for the world to experience. You will be a part of the entire process and this process leads to communitcating with us throughout each step so that the end product is something you're happy with and something we can be proud of.</p>
+      </div>
+    </section>
     </>
   );
 };
--- innovo-pro-web-design/src/Components/NavBar/NavBar.jsx
+++ innovo-pro-web-design/src/Components/NavBar/NavBar.jsx
@@ -6,15 +6,17 @@ const NavBar = () => {
 
     return (
         <>
-        <div className="NavBar">
-            <img src={CompanyLogo} alt="Company Logo which is a geometric logo showing the letters I and P with a gradient of various shades of green." />
-            <div className="CenterNav">
-                <a className="NavItem" href="">Our Process</a> {/*Need to figure out a JS Script to allow these buttons to automatically scroll to each section. (Note -> Check the code for the Steven King Design Show website to see how you did it.)*/}
-                <a className="NavItem" href="">Work</a>
-                <a className="NavItem" href="">About</a>
-                <a className="NavItem" href="">Pricing</a>
+        <div className="CenteringNav">
+            <div className="NavBar">
+                <a className="Logo" href="/"><img src={CompanyLogo} alt="Company Logo which is a geometric logo showing the letters I and P with a gradient of various shades of green." /></a>
+                <div className="CenterNav">
+                    <a className="NavItem" href="">Our Process</a> {/*Need to figure out a JS Script to allow these buttons to automatically scroll to each section. (Note -> Check the code for the Steven King Design Show website to see how you did it.)*/}
+                    <a className="NavItem" href="">Work</a>
+                    <a className="NavItem" href="">About</a>
+                    <a className="NavItem" href="">Pricing</a>
+                </div>
+                <a className="CTA" href="/contact">Book a Call!</a>
             </div>
-            <a className="CTA" href="/contact">Book a Call!</a>
         </div>
         </>
     )
